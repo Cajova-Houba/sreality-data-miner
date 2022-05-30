533e039bfd1d1584d27bc834ddd8eea78200e642
Cajova Houba <tkmushroomer@gmail.com>
2022-05-30 22:56:06 +0200

    Script encoding, logging.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8
 from sreality_crawler import Crawler
 from sreality_dao import SRealityDao
 from sreality_parser import Parser
 import configparser
+import logging
+
+logging.basicConfig(filename='crawler.log', encoding='utf-8', level=logging.DEBUG)
+logger = logging.getLogger("crawler_main")
+
 
 def loadDbConfig(fileName):
     config = configparser.ConfigParser()
@@ -9,12 +15,28 @@ def loadDbConfig(fileName):
     return config['Database']
 
 def main():
+    logger.info("Starting crawler")
     dbConfig = loadDbConfig("config.ini")
+    logger.info("Config parsed")
     dao = SRealityDao(dbConfig)
     parser = Parser()
     crawler = Crawler(dao, parser)
 
-    crawler.searchNewOffers()
-    crawler.processNewOffers()
+    logger.info("Searching for new offers")
+    try:
+        crawler.searchNewOffers()
+    except:
+        logger.exception("Unexpected exception while searching for new offers:")
+        return
+
+    logger.info("Processing new offers")
+    try:
+        crawler.processNewOffers()
+    except:
+        logger.exception("Unexpected exception while processing new offers:")
+        return
+
+    logger.info("Done, exiting crawler")
+
 
 main()
